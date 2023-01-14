894ec71811a64700100238423727501e3d0cdcf3
SMOS-Github <SMOS-Github@users.noreply.github.com>
2023-01-15 01:17:08 +0530

    New Implement
    Set buttons and add new script

---
+++ Assets/Scripts/MenuHandeler.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHandeler : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        StartGame();
+        QuitGame();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+
+    }
+    public void QuitGame()
+    {
+
+    }
+   
+
+}
